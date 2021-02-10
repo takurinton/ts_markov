7b1a6a267334ccf97ea3e7c7396a62d4ab90e3aa
takurinton <taryochanmannn@icloud.com>
2021-02-10 12:32:43 +0900

    add: arrangeText

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import text from './text';
 
+const arrangeText = (text: string) => {
+    text = text.replace(/\n/g, '。');
+    text = text.replace(/[\?\!？！]/g, '。');
+    text = text.replace(/[-|｜:：・]/g, '。');
+    text = text.replace(/[「」（）\(\)\[\]【】]/g, ' ');
+    return text;
+};
+
+console.log(arrangeText(text));
