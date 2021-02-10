e8025f7a7239bce99c3fcf5408800c8f318097f1
takurinton <taryochanmannn@icloud.com>
2021-02-11 00:38:03 +0900

    add: make sentence

--- src/index.ts
+++ src/index.ts
@@ -28,16 +28,37 @@ const makeDict = (text: string): Morpheme => {
         if (tokenized[0]) morpheme[START].push(tokenized[0]); // 文の先頭の判断
         for (var w = 0; w < tokenized.length; w++) {
             const now: string = tokenized[w];
-            const next: string = tokenized[w+1] ?? END; // nextがなかったら終了
+            const next: string = tokenized[w+1] ?? END; 
+            // ここではSTART判断と同じことをしてる
+            // 今のワードに紐づく次のワードを突っ込んでる
             if (!morpheme[now]) morpheme[now] = [];
             morpheme[now].push(next);
+
             if (now === '、') morpheme[START].push(next); // 、はstartの要素として使えるっぽい
         };
     };
     return morpheme;
 };
 
+const makeSentence = (dict: Morpheme): string => {
+    let now: string = '';
+    let result: string = '';
+    now = dict[START][Math.floor(Math.random() * dict[START].length)]; // いい感じにしてる(ぇ
+    result += now;
+    while (now !== END) {
+        console.log(now);
+        now = dict[START][Math.floor(Math.random() * dict[now].length)];
+        result += now;
+    }
+    result = result.replace(END, '。')
+    return result;
+}
 
-const _text: string = arrangeText(text);
-const dict: Morpheme = makeDict(_text);
-console.log(dict);
+const main = () => {
+    const _text: string = arrangeText(text);
+    const dict: Morpheme = makeDict(_text);
+    const sentence: string = makeSentence(dict);
+    console.log(sentence);
+}
+
+main();
